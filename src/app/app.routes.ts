5f0ff263339ec15c1b987d485378c9c9c4539a45
MarwaMohamed299 <128231159+MarwaMohamed299@users.noreply.github.com>
2025-06-08 14:01:51 +0300

    Adding Project Structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,36 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './Components/AuthModule/login/login.component';
+import { RegisterComponent } from './Components/AuthModule/register/register.component';
+import { UserDetailsComponent } from './Components/AuthModule/user-details/user-details.component';
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { 
+    path: 'register', component : RegisterComponent},
+    
+  
+  { 
+    path: 'user-details', component: UserDetailsComponent
+  },
+  {
+    path: 'departments',
+    loadChildren: () => 
+      import('./Components/departmentsModule/departments.module').then(
+        m => m.DepartmentsModule
+      )
+  },
+  {
+    path: 'jobs',
+    loadChildren: () => 
+      import('./Components/jobs-module/jobs-module.module').then(
+        m => m.JobsModuleModule
+      )
+  }
+];
 
-export const routes: Routes = [];
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
